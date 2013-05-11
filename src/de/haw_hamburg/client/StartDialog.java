e7728a851151845261bc5ff19fb4b6d1d4fd9da5
Patrick Detlefsen <patrick.detlefsen@gmail.com>
2013-05-11 14:45:53 +0200

    set text in an error label in the start dialog when input not valid

--- src/de/haw_hamburg/client/StartDialog.java
+++ src/de/haw_hamburg/client/StartDialog.java
@@ -35,6 +35,7 @@ public class StartDialog extends javax.swing.JDialog {
         jLabel2 = new javax.swing.JLabel();
         connectButton = new javax.swing.JButton();
         jLabel3 = new javax.swing.JLabel();
+        errorLabel = new javax.swing.JLabel();
 
         jLabel1.setText("jLabel1");
 
@@ -54,6 +55,8 @@ public class StartDialog extends javax.swing.JDialog {
         jLabel3.setFont(new java.awt.Font("Lucida Grande", 1, 16)); // NOI18N
         jLabel3.setText("HAW Chat");
 
+        errorLabel.setForeground(new java.awt.Color(204, 0, 0));
+
         org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -61,30 +64,34 @@ public class StartDialog extends javax.swing.JDialog {
             .add(layout.createSequentialGroup()
                 .addContainerGap()
                 .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                    .add(layout.createSequentialGroup()
-                        .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                            .add(layout.createSequentialGroup()
-                                .add(serverHostnameLabel)
-                                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED))
-                            .add(org.jdesktop.layout.GroupLayout.TRAILING, layout.createSequentialGroup()
-                                .add(jLabel2)
-                                .add(27, 27, 27)))
-                        .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                            .add(chatUsernameTextfield, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 290, Short.MAX_VALUE)
-                            .add(serverHostnameTextField)))
                     .add(org.jdesktop.layout.GroupLayout.TRAILING, layout.createSequentialGroup()
                         .add(0, 0, Short.MAX_VALUE)
                         .add(connectButton))
                     .add(layout.createSequentialGroup()
-                        .add(jLabel3)
-                        .add(0, 0, Short.MAX_VALUE)))
+                        .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                            .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                                .add(layout.createSequentialGroup()
+                                    .add(serverHostnameLabel)
+                                    .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED))
+                                .add(org.jdesktop.layout.GroupLayout.TRAILING, layout.createSequentialGroup()
+                                    .add(jLabel2)
+                                    .add(27, 27, 27)))
+                            .add(layout.createSequentialGroup()
+                                .add(jLabel3)
+                                .add(42, 42, 42)))
+                        .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                            .add(chatUsernameTextfield, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 290, Short.MAX_VALUE)
+                            .add(serverHostnameTextField)
+                            .add(org.jdesktop.layout.GroupLayout.TRAILING, errorLabel, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
             .add(layout.createSequentialGroup()
                 .add(14, 14, 14)
-                .add(jLabel3)
+                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING, false)
+                    .add(jLabel3, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .add(errorLabel, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
                 .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
                     .add(serverHostnameTextField, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
@@ -105,16 +112,20 @@ public class StartDialog extends javax.swing.JDialog {
         // 1. set button as diabled
         connectButton.setEnabled(false);
         // 2. validate that both fields contain text
-        if (serverHostnameTextField.getText() == "" || chatUsernameTextfield.getText() == "") {
+        if (serverHostnameTextField.getText().length() <= 0 || chatUsernameTextfield.getText().length() <= 0) {
+            errorLabel.setText("Beide Textfelder müssen ausgefullt werden.");
+            connectButton.setEnabled(true);
+        } else if (chatUsernameTextfield.getText().length() > 20) {
+            errorLabel.setText("Der Chatname darf nicht länger als 20 Zeichen sein.");
             connectButton.setEnabled(true);
         } else {
-        // 3. read text from hostnameField
+            // 3. read text from hostnameField
             getClient().setServerHostname(serverHostnameTextField.getText());
-        // 4. read text from userNameField
+            // 4. read text from userNameField
             getClient().setUserName(chatUsernameTextfield.getText());
-        // 5. close dialog
+            // 5. close dialog
             this.setVisible(false);
-        // 7. launch gui in client
+            // 7. launch gui in client
             getClient().startDialogFinished();
         }
     }//GEN-LAST:event_connectButtonActionPerformed
@@ -163,6 +174,7 @@ public class StartDialog extends javax.swing.JDialog {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextField chatUsernameTextfield;
     private javax.swing.JButton connectButton;
+    private javax.swing.JLabel errorLabel;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
